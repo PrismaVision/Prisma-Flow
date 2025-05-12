82e8a1a23e16f97bb80cf2c24e7a37bdff6e3fda
UitaloAndrade <uitalo.a.reis@gmail.com>
2025-05-12 20:29:41 -0300

    initial settings

---
+++ src/main/java/com/prismaflow/manager/PrismaflowApplication.java
@@ -0,0 +1,13 @@
+package com.prismaflow.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrismaflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrismaflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prismaflow/manager/PrismaflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prismaflow.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismaflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
