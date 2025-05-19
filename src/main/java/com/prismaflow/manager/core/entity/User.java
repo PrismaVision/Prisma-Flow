4d8a17fbf7cd5ff3d3e484cf401c91c254dc1f87
SamuelSoaresSilva <samuelsoaressilva.dev@gmail.com>
2025-05-19 19:00:33 -0300

    cria core gateway

--- src/main/java/com/prismaflow/manager/core/entity/User.java
+++ src/main/java/com/prismaflow/manager/core/entity/User.java
@@ -1,12 +1,11 @@
 package com.prismaflow.manager.core.entity;
 
 public record User(
-
+        
         Long id,
         String name,
         String email,
         String password,
         String role
-
 ) {
 }
---
+++ src/main/java/com/prismaflow/manager/core/gateway/Gateway.java
@@ -0,0 +1,5 @@
+package com.prismaflow.manager.core.gateway;
+
+public class Gateway {
+
+}
