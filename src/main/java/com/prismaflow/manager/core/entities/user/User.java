ddb01eecbc6a6dc450237a01f7dd325ed0c529d7
Samuel Soares <samuel.soares.cunha64@gmail.com>
2025-05-21 13:40:42 -0300

    [Refactor] reorganize folders

--- src/main/java/com/prismaflow/manager/core/entities/user/User.java
+++ src/main/java/com/prismaflow/manager/core/entities/user/User.java
@@ -1,7 +1,8 @@
 package com.prismaflow.manager.core.entities.user;
 
+import com.prismaflow.manager.core.enums.UserRole;
+
 public record User(
-        
         Long id,
         String name,
         String email,
